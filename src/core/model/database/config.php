1f42e929961c52c85ba5ce51fd23bbd3179ba0f9
wassimhazime <wassim.hazime@gmail.com>
2017-09-22 13:56:39 +0200

    config json file

--- src/core/model/database/config.php
+++ src/core/model/database/config.php
@@ -3,9 +3,22 @@
 namespace core\model\database;
 use app\config\configdb;
 
-class  config extends configDB{
+class  config {
    
-  
+    function __construct() {
+        
     
+                       $stringconfig = file_get_contents(ROOT.D_S."src".D_S."app".D_S."config".D_S."database.json");
+                       $json_config = json_decode($stringconfig, true);
+
+            
+                            $this->DB = $json_config['$DB'];
+                            $this->dbhost = $json_config['$dbhost'];
+                            $this->dbuser = $json_config['$dbuser'];
+                            $this->dbpass = $json_config['$dbpass'];
+                            $this->dbname = $json_config['$dbname'];
+        
+    }
+
     
 }
